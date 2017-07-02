b02ebeb81f92422e886f277af6ea87ed4036d026
shatter15 <wucheng15@mails.tsinghua.edu.cn>
2017-07-02 15:09:07 +0800

    Update urls.py

--- EatKing/urls.py
+++ EatKing/urls.py
@@ -3,8 +3,8 @@ from . import views,auth_views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^search', views.search, name='search'),
-    url(r'^search/([\w]+)$',views.search_submit,name='search_submit'),
+    url(r'^search$', views.search, name='search'),
+    url(r'^search/([\w\ ]*)$', views.search_submit, name='search_submit'),
     url(r'^shop/([1-9][0-9]*)$',views.enter_shop,name='enter_shop'),
     url(r'^shop/([1-9][0-9]*)/comment$',views.comment,name = 'comment'),
     url(r'^shop/([1-9][0-9]*)/like_comment$',views.like_comment,name = 'like_commment'),
@@ -13,8 +13,8 @@ urlpatterns = [
     url(r'^shop/([1-9][0-9]*)/unlike_shop$', views.unlike_shop, name='unlike_shop'),
 
 # auth
-	url(r'^login$', auth_views.login, name='login'),
-	url(r'^authenticate$', auth_views.authenticate, name='authenticate'),
+    url(r'^login$', auth_views.login, name='login'),
+    url(r'^authenticate$', auth_views.authenticate, name='authenticate'),
     url(r'^signup$', auth_views.signup, name='signup'),
     url(r'^signup/submit$', auth_views.signup_submit, name='signup-submit'),
     url(r'^logout$', auth_views.logout, name='logout'),
