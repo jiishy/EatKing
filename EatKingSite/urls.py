43d81ad231dcd5b3c6bbefc74e4266dd833ae63f
shatter15 <wucheng15@tsinghua.mails.edu.cn>
2017-07-04 19:05:11 +0800

    middle check

--- EatKingSite/urls.py
+++ EatKingSite/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 #from . import views
+
 admin.autodiscover()
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('EatKing.urls')),
     #url(r'^$', views.index, name='index'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
